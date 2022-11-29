108773ea354d9714e0ddb8775ea6479ce3dd8f3f
Val <val.samonte@gmail.com>
2022-11-30 04:15:07 +0800

    BuyPage: mobile layout

--- src/pages/BuyPage.tsx
+++ src/pages/BuyPage.tsx
@@ -1,7 +1,7 @@
 export function BuyPage() {
   return (
-    <div className='lg:py-10 px-5 flex flex-col gap-5'>
-      <ul className='flex items-center gap-3 flex-wrap'>
+    <div className='px-5 flex flex-col gap-5'>
+      <ul className='py-5 flex items-center gap-3 flex-wrap'>
         <li>Buy </li>
         <li className='bg-slate-100 text-zinc-900 px-2 py-1 rounded-xl flex items-center'>
           <img src='/solanaLogoMark.svg' className='h-4 w-4 mr-2' />
@@ -22,7 +22,7 @@ export function BuyPage() {
         <li>in PHP</li>
       </ul>
       <ul className='flex flex-col gap-5'>
-        <li className='p-5 bg-slate-100 text-zinc-900 rounded-2xl grid grid-cols-5 items-center'>
+        <li className='gap-3 cursor-pointer transition-all p-5 hover:p-7 hover:-m-2 bg-slate-100 text-zinc-900 rounded-2xl grid grid-cols-2 md:grid-cols-5 items-center'>
           <div className='flex flex-col'>
             <div className='flex items-center'>
               <span className='underline'>Dt29...wzHC</span>
@@ -34,7 +34,7 @@ export function BuyPage() {
             </div>
           </div>
           <div className='flex flex-col'>
-            <div className='flex items-center'>
+            <div className='flex items-center justify-end md:justify-center'>
               <span className='font-bold text-2xl'>800.00</span>
               <span className='ml-2'>PHP</span>
             </div>
@@ -42,7 +42,7 @@ export function BuyPage() {
           <div className='flex flex-col col-span-2'>
             <div className='flex items-center'>
               <span>Available</span>
-              <span className='flex-auto border-b border-b-zinc-300 border-dashed mx-5' />
+              <span className='flex-auto border-b border-b-zinc-200 border-dashed mx-1 lg:mx-5' />
               <span className='flex items-center justify-end'>
                 <img src='/solanaLogoMark.svg' className='h-4 w-4 mr-2' />
                 <span className='font-bold'>375.00</span>
@@ -50,7 +50,7 @@ export function BuyPage() {
             </div>
             <div className='flex items-center'>
               <span>Limit</span>
-              <span className='flex-auto border-b border-b-zinc-300 border-dashed mx-5' />
+              <span className='flex-auto border-b border-b-zinc-200 border-dashed mx-1 lg:mx-5' />
               <span className='flex items-center justify-end'>
                 <span className='font-bold'>200,000.00</span>
                 <span className='mx-2'>-</span>
@@ -59,7 +59,7 @@ export function BuyPage() {
               </span>
             </div>
           </div>
-          <div className='flex items-center justify-end'>
+          <div className='flex items-center justify-end col-span-2 md:col-span-1'>
             <ul className='flex flex-wrap gap-2 text-sm justify-end'>
               <li className='bg-blue-300 px-2 py-1 rounded-xl'>GCash</li>
               <li className='bg-orange-300 px-2 py-1 rounded-xl'>Union Bank</li>
