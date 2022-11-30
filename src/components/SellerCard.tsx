3f088c8d11e6b4a628fd29105e2f82b8b09c7f59
Val <val.samonte@gmail.com>
2022-11-30 19:15:15 +0800

    Separate SellerCard component

---
+++ src/components/SellerCard.tsx
@@ -0,0 +1,48 @@
+export function SellerCard() {
+  return (
+    <li className='gap-3 cursor-pointer transition-all p-5 hover:p-7 hover:-m-2 bg-slate-100 text-zinc-900 rounded-2xl grid grid-cols-2 md:grid-cols-5 items-center'>
+      <div className='flex flex-col'>
+        <div className='flex items-center'>
+          <span className='underline'>Dt29...wzHC</span>
+          <span className='bg-green-500 ml-2 h-3 w-3 rounded-full inline-block'></span>
+        </div>
+        <div className='flex items-center'>
+          <img src='/discord_black.svg' className='h-4 w-4 mr-2' />
+          Profile
+        </div>
+      </div>
+      <div className='flex flex-col'>
+        <div className='flex items-center justify-end md:justify-center'>
+          <span className='font-bold text-2xl'>800.00</span>
+          <span className='ml-2'>PHP</span>
+        </div>
+      </div>
+      <div className='flex flex-col col-span-2'>
+        <div className='flex items-center'>
+          <span>Available</span>
+          <span className='flex-auto border-b border-b-zinc-200 border-dashed mx-2 lg:mx-5' />
+          <span className='flex items-center justify-end'>
+            <img src='/solanaLogoMark.svg' className='h-4 w-4 mr-2' />
+            <span className='font-bold'>375.00</span>
+          </span>
+        </div>
+        <div className='flex items-center'>
+          <span>Limit</span>
+          <span className='flex-auto border-b border-b-zinc-200 border-dashed mx-2 lg:mx-5' />
+          <span className='flex items-center justify-end'>
+            <span className='font-bold'>200,000.00</span>
+            <span className='mx-2'>-</span>
+            <span className='font-bold'>300,000.00</span>
+            <span className='ml-2'>PHP</span>
+          </span>
+        </div>
+      </div>
+      <div className='flex items-center justify-end col-span-2 md:col-span-1'>
+        <ul className='flex flex-wrap gap-2 text-xs justify-end'>
+          <li className='bg-blue-300 px-2 py-1 rounded-xl'>GCash</li>
+          <li className='bg-orange-300 px-2 py-1 rounded-xl'>Union Bank</li>
+        </ul>
+      </div>
+    </li>
+  )
+}
--- src/pages/BuyPage.tsx
+++ src/pages/BuyPage.tsx
@@ -1,7 +1,9 @@
+import { SellerCard } from '@/components/SellerCard'
+
 export function BuyPage() {
   return (
     <div className='px-5 flex flex-col gap-5'>
-      <ul className='py-5 flex items-center gap-3 flex-wrap'>
+      <ul className='py-5 md:pt-10 md:pb-0 flex items-center gap-3 flex-wrap'>
         <li>Buy </li>
         <li className='bg-slate-100 text-zinc-900 px-2 py-1 rounded-xl flex items-center'>
           <img src='/solanaLogoMark.svg' className='h-4 w-4 mr-2' />
@@ -22,50 +24,7 @@ export function BuyPage() {
         <li>in PHP</li>
       </ul>
       <ul className='flex flex-col gap-5'>
-        <li className='gap-3 cursor-pointer transition-all p-5 hover:p-7 hover:-m-2 bg-slate-100 text-zinc-900 rounded-2xl grid grid-cols-2 md:grid-cols-5 items-center'>
-          <div className='flex flex-col'>
-            <div className='flex items-center'>
-              <span className='underline'>Dt29...wzHC</span>
-              <span className='bg-green-500 ml-2 h-3 w-3 rounded-full inline-block'></span>
-            </div>
-            <div className='flex items-center'>
-              <img src='/discord_black.svg' className='h-4 w-4 mr-2' />
-              Profile
-            </div>
-          </div>
-          <div className='flex flex-col'>
-            <div className='flex items-center justify-end md:justify-center'>
-              <span className='font-bold text-2xl'>800.00</span>
-              <span className='ml-2'>PHP</span>
-            </div>
-          </div>
-          <div className='flex flex-col col-span-2'>
-            <div className='flex items-center'>
-              <span>Available</span>
-              <span className='flex-auto border-b border-b-zinc-200 border-dashed mx-1 lg:mx-5' />
-              <span className='flex items-center justify-end'>
-                <img src='/solanaLogoMark.svg' className='h-4 w-4 mr-2' />
-                <span className='font-bold'>375.00</span>
-              </span>
-            </div>
-            <div className='flex items-center'>
-              <span>Limit</span>
-              <span className='flex-auto border-b border-b-zinc-200 border-dashed mx-1 lg:mx-5' />
-              <span className='flex items-center justify-end'>
-                <span className='font-bold'>200,000.00</span>
-                <span className='mx-2'>-</span>
-                <span className='font-bold'>300,000.00</span>
-                <span className='ml-2'>PHP</span>
-              </span>
-            </div>
-          </div>
-          <div className='flex items-center justify-end col-span-2 md:col-span-1'>
-            <ul className='flex flex-wrap gap-2 text-sm justify-end'>
-              <li className='bg-blue-300 px-2 py-1 rounded-xl'>GCash</li>
-              <li className='bg-orange-300 px-2 py-1 rounded-xl'>Union Bank</li>
-            </ul>
-          </div>
-        </li>
+        <SellerCard />
       </ul>
     </div>
   )
